f8673818db90f7a46185eb80cc47aefcd7d9a11b
jatv93 <toledojennifer20@gmail.com>
2020-05-29 02:50:40 +0000

    final commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export class TrafficLight extends Component {
+	constructor() {
+		super();
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+		this.state = {
+			redStatus: "red-off",
+			yellowStatus: "yellow-off",
+			greenStatus: "green-off"
+		};
+	}
+
+	cambiaColorRed() {
+		this.setState({
+			redStatus: "red-on",
+			yellowStatus: "yellow-off",
+			greenStatus: "green-off"
+		});
+	}
+
+	cambiaColorYellow() {
+		this.setState({
+			redStatus: "red-off",
+			yellowStatus: "yellow-on",
+			greenStatus: "green-off"
+		});
+	}
+
+	cambiaColorGreen() {
+		this.setState({
+			redStatus: "red-off",
+			yellowStatus: "yellow-off",
+			greenStatus: "green-on"
+		});
+	}
+
+	render() {
+		return (
+			<div className="traffic-light">
+				<div
+					className={this.state.redStatus}
+					onClick={() => {
+						this.cambiaColorRed();
+					}}
+				/>
+				<div
+					className={this.state.yellowStatus}
+					onClick={() => {
+						this.cambiaColorYellow();
+					}}
+				/>
+				<div
+					className={this.state.greenStatus}
+					onClick={() => {
+						this.cambiaColorGreen();
+					}}
+				/>
+			</div>
+		);
+	}
 }
